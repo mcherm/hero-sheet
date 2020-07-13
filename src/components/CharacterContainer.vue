388369ce99144e15e0d2d41a61a12543d82128aa
Michael Chermside <mcherm@mcherm.com>
2020-07-13 17:56:25 -0400

    Failed attempts to fix a bug in updates.

--- src/components/CharacterContainer.vue
+++ src/components/CharacterContainer.vue
@@ -23,6 +23,8 @@
   import {updaterClasses, newUpdaterFromActiveEffect, UnsupportedUpdaterInActiveEffectError} from "../js/updaters.js";
   import {getCharacter, saveCharacter, NotLoggedInError} from "../js/api.js";
   import {removeActiveEffects} from "../js/heroSheetUtil.js";
+  import {newBlankPower, newHsid} from "../js/heroSheetVersioning"; // FIXME: Remove after testing
+  import Vue from "vue";
 
   export default {
     name: "CharacterContainer",
@@ -52,6 +54,7 @@
           return null; // No dialog; just move on
         }
       };
+      window.characterContainer = this; // FIXME: Remove
     },
     mounted: function() {
       this.$globals.eventBus.$on("new-updater", this.createUpdater);
@@ -111,7 +114,125 @@
           this.saveCharacter();
         }, SAVE_FREQUENCY_MILLIS);
       },
+      // FIXME: Remove this
+      // This is an experiment to see when watches trigger
+      performWatchExperiment(charsheet) {
+
+        console.log(`Experiment 1 begins`);
+        const powers1 = charsheet.powers;
+        const newPower1 = newBlankPower();
+        powers1.push(newPower1);
+        Vue.set(newPower1, "name", newHsid());
+        Vue.set(newPower1, "effect", "Enhanced Trait");
+        Vue.set(newPower1, "option", "Enhanced Dexterity");
+        const cancelFunction1 = this.$watch(
+          () => {
+            console.log(`Experiment 1 watch is triggering`);
+            const result = {
+              identity: {
+                powerCount: charsheet.powers.length,
+                powerExists: newPower1 !== null,
+                powerHsid: newPower1 === null ? null : newPower1.hsid,
+                powerEffect: newPower1 === null ? null : newPower1.effect,
+                powerOption: newPower1 === null ? null : newPower1.option,
+                //listIAmIn: charsheet.powers,
+                // charsheetItself: charsheet,
+              },
+              calculations: {
+                powerRanks: newPower1 === null ? null : newPower1.ranks
+              }
+            };
+            console.log(`Experiment 1 watch returning ${JSON.stringify(result)}`);
+            return result;
+          },
+          (newValue, oldValue) => {
+            console.log(`Experiment 1 watch processing()`);
+          },
+          {deep: true} // FIXME: Do I really need this? I'm not sure!!
+        );
+        console.log(`Experiment 1 Will Now Delete The Power`);
+        this.$delete(powers1, powers1.length - 1);
+        console.log(`Experiment 1 ends`);
+
+        console.log(`Experiment 2 begins`);
+        const powers2 = charsheet.powers;
+        const newPower2 = newBlankPower();
+        powers2.push(newPower2);
+        Vue.set(newPower2, "name", newHsid());
+        Vue.set(newPower2, "effect", "Enhanced Trait");
+        Vue.set(newPower2, "option", "Enhanced Dexterity");
+        Vue.set(newPower2, "innerList", ["a", "b", "c"]);
+        const cancelFunction2 = this.$watch(
+          () => {
+            console.log(`Experiment 2 watch is triggering`);
+            const result = {
+              identity: {
+                powerCount: powers2.length,
+                powerExists: newPower2 !== null,
+                powerHsid: newPower2 === null ? null : newPower2.hsid,
+                powerEffect: newPower2 === null ? null : newPower2.effect,
+                powerOption: newPower2 === null ? null : newPower2.option,
+                listOfNames: powers2.map(x => x.name),
+                innerListNames: newPower2.innerList.map(x => x),
+              },
+              calculations: {
+                powerRanks: newPower2 === null ? null : newPower2.ranks
+              }
+            };
+            console.log(`Experiment 2 watch returning ${JSON.stringify(result)}`);
+            return result;
+          },
+          (newValue, oldValue) => {
+            console.log(`Experiment 2 watch processing()`);
+          }
+          //{deep: true, immediate: true} // FIXME: Do I really need this? I'm not sure!!
+        );
+        console.log(`Experiment 2 Will Now Modify the inner list`);
+        newPower2.innerList.pop();
+        console.log(`Experiment 2 ends`);
+
+        console.log(`Experiment 3 begins`);
+        const powers3 = charsheet.powers;
+        const newPower3 = newBlankPower();
+        powers3.push(newPower3);
+        Vue.set(newPower3, "name", newHsid());
+        Vue.set(newPower3, "effect", "Enhanced Trait");
+        Vue.set(newPower3, "option", "Enhanced Dexterity");
+        const cancelFunction3 = this.$watch(
+          () => {
+            console.log(`Experiment 3 watch is triggering`);
+            const result = {
+              identity: {
+                powerCount: powers3.length,
+                powerExists: newPower3 !== null,
+                powerHsid: newPower3 === null ? null : newPower3.hsid,
+                powerEffect: newPower3 === null ? null : newPower3.effect,
+                powerOption: newPower3 === null ? null : newPower3.option,
+                listOfNames: powers3.map(x => x.name),
+              },
+              calculations: {
+                powerRanks: newPower3 === null ? null : newPower3.ranks
+              }
+            };
+            console.log(`Experiment 3 watch returning ${JSON.stringify(result)}`);
+            return result;
+          },
+          (newValue, oldValue) => {
+            console.log(`Experiment 3 watch processing()`);
+          }
+          //{deep: true, immediate: true} // FIXME: Do I really need this? I'm not sure!!
+        );
+        console.log(`Experiment 3 Will Now Modify The Power`);
+        this.$set(newPower3, "option", "Enhanced Stamina");
+        // this.$delete(powers3, powers3.length - 1);
+        console.log(`Experiment 3 ends`);
+
+      },
       installUpdaters: function(charsheet) {
+        if(true) {  // FIXME: Remove this
+          //this.performWatchExperiment(charsheet);
+          return;
+        }
         for (const stat in statsData) {
           new updaterClasses["StatRankUpdater"](this, charsheet, stat);
         }
