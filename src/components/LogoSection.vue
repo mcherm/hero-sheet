d575908fa49b7f93ab6a67b39d4500f6eb749fe4
Michael Chermside <mcherm@mcherm.com>
2020-09-16 22:37:44 -0400

    Converted to a multi-page project, and started creating a page for processing the password reset.

---
+++ src/components/LogoSection.vue
@@ -0,0 +1,50 @@
+<template functional>
+  <div class="logo-section">
+    <div class="text-logo">
+      <span class="level-1">Hero-Sheet</span>
+      <span class="level-3"> : Mutants and Masterminds</span>
+      <span class="level-2"> : Open for Alpha Testing</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'logo-section'
+  }
+</script>
+
+<style scoped>
+  .header button {
+    border: solid 1px var(--box-border-color);
+    padding: 2px;
+    margin: 4px 2px;
+  }
+
+  .logo-section {
+    background-color: var(--title-color);
+    color: rgb(255, 255, 255);
+    border: solid 1px var(--box-border-color);
+    padding-top: 4px;
+    padding-left: 4px;
+    padding-right: 4px;
+    padding-bottom: 2px;
+  }
+
+  .text-logo {
+    text-align: center;
+    font-size: x-large;
+  }
+
+  @media (max-width: 750px) {
+    .level-3 {
+      display: none;
+    }
+  }
+
+  @media (max-width: 500px) {
+    .level-2 {
+      display: none;
+    }
+  }
+</style>
--- src/pages/reset-password/main.js
+++ src/pages/reset-password/main.js
@@ -2,33 +2,33 @@ import Vue from 'vue'
 import App from './App.vue'
 
 // Pre-register all components that will be involved in recursion or are widely used
-import PowerList from "./components/PowerList.vue"
+import PowerList from "@/components/PowerList.vue"
 Vue.component("PowerList", PowerList);
-import Power from "./components/Power.vue"
+import Power from "@/components/Power.vue"
 Vue.component("Power", Power);
-import NumberDisplay from "./components/NumberDisplay.vue"
+import NumberDisplay from "@/components/NumberDisplay.vue"
 Vue.component("NumberDisplay", NumberDisplay);
-import ModifiableNumberDisplay from "./components/ModifiableNumberDisplay.vue"
+import ModifiableNumberDisplay from "@/components/ModifiableNumberDisplay.vue"
 Vue.component("ModifiableNumberDisplay", ModifiableNumberDisplay);
-import NumberEntry from "./components/NumberEntry.vue"
+import NumberEntry from "@/components/NumberEntry.vue"
 Vue.component("NumberEntry", NumberEntry);
-import StringEntry from "./components/StringEntry.vue"
+import StringEntry from "@/components/StringEntry.vue"
 Vue.component("StringEntry", StringEntry);
-import YesNoToggle from "./components/YesNoToggle.vue";
+import YesNoToggle from "@/components/YesNoToggle.vue";
 Vue.component("YesNoToggle", YesNoToggle);
-import BoxedSection from "./components/BoxedSection.vue"
+import BoxedSection from "@/components/BoxedSection.vue"
 Vue.component("BoxedSection", BoxedSection);
-import ModalLightbox from "./components/ModalLightbox.vue";
+import ModalLightbox from "@/components/ModalLightbox.vue";
 Vue.component("ModalLightbox", ModalLightbox);
-import PencilIcon from "./components/PencilIcon.vue";
+import PencilIcon from "@/components/PencilIcon.vue";
 Vue.component("PencilIcon", PencilIcon);
-import TrashIcon from "./components/TrashIcon.vue";
+import TrashIcon from "@/components/TrashIcon.vue";
 Vue.component("TrashIcon", TrashIcon);
-import DocsLookup from "./components/DocsLookup.vue";
+import DocsLookup from "@/components/DocsLookup.vue";
 Vue.component("DocsLookup", DocsLookup);
 
 const app = new Vue({
-  el: "#app",
+  el: "#reset-password",
   render: h => h(App)
 });
 Vue.config.devtools = true;
