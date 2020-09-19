23345e4c17797376c84a730ac1467496dccbd76e
Michael Chermside <mcherm@mcherm.com>
2020-09-19 08:25:17 -0400

    Better look for fields of a read-only charsheet.

--- src/components/YesNoToggle.vue
+++ src/components/YesNoToggle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="yn-toggle" :class="{'disabled': !isReallyMutable}" @click="toggle()">{{value ? "Yes" : "No"}}</div>
+  <div class="yn-toggle" :class="{'disabled': isDisabled, 'read-only': isReadOnly}" @click="toggle()">{{value ? "Yes" : "No"}}</div>
 </template>
 
 <script>
@@ -11,14 +11,17 @@
       mutable: { type: Boolean, requred: false, default: true },
     },
     computed: {
-      isReallyMutable: function() {
+      isDisabled: function() {
+        return !this.mutable;
+      },
+      isReadOnly: function() {
         const globalReadOnly = this.editModes && ("isReadOnly" in this.editModes) && this.editModes.isReadOnly;
-        return this.mutable && !globalReadOnly;
+        return globalReadOnly;
       }
     },
     methods: {
       toggle: function() {
-        if (this.isReallyMutable) {
+        if (!this.isDisabled && !this.isReadOnly) {
           this.$emit("input", !this.value);
         }
       }
@@ -31,6 +34,10 @@
     background-color: var(--entry-field);
   }
   .yn-toggle.disabled {
-    background-color: inherit;
+    background-color: var(--paper-color);
+    color: grey;
+  }
+  .yn-toggle.read-only {
+    background-color: var(--paper-color);
   }
 </style>
