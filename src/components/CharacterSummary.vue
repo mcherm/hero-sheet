619c1d61059c801ed9b63e375055662c37c0577b
Michael Chermside <mcherm@mcherm.com>
2020-11-19 20:47:09 -0500

    Basic support for actually generating and downloading a PDF (ONLY in dev mode!)

--- src/components/CharacterSummary.vue
+++ src/components/CharacterSummary.vue
@@ -6,13 +6,27 @@
     <div>
       strength = {{getCharsheet().abilities.strength.ranks}}
     </div>
+    <edit-button :on-click="() => {openPDF();}" :is-navigation="true">Show PDF</edit-button>
+    <edit-button :on-click="() => {downloadPDF();}" :is-navigation="true">Download PDF</edit-button>
   </div>
 </template>
 
 <script>
+  import {documentDefinition} from "../js/pdf.js";
+
+  const pdfMake = require("pdfmake/build/pdfmake.js");
+
   export default {
     name: "CharacterSummary",
     inject: ["getCharsheet"],
+    methods: {
+      openPDF: function() {
+        pdfMake.createPdf(documentDefinition(this.getCharsheet())).open();
+      },
+      downloadPDF: function() {
+        pdfMake.createPdf(documentDefinition(this.getCharsheet())).download();
+      },
+    }
   }
 </script>
 
