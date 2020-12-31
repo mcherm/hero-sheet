b01a30917767553a1b84ed93cdd25a2f8e4ec0e6
Michael Chermside <mcherm@mcherm.com>
2020-12-30 22:21:05 -0500

    Added RanksAndMeasures component.

--- src/components/AboutApplication.vue
+++ src/components/AboutApplication.vue
@@ -14,6 +14,7 @@
     <p>If you have any feedback or suggestions, send an email to <a href="mailto:hero-sheet@mcherm.com">hero-sheet@mcherm.com</a>.</p>
     <h2>Recent Changes</h2>
     <ul class="change-list scrolling-list">
+      <li>2020-12-30: Added Ranks and Measures to In-Play tab.</li>
       <li>2020-12-26: Fixed bug when a sense with qualities is deleted and also the names of adjustments from powers.</li>
       <li>2020-12-26: The ability to turn off powers. Supports linked and alternate arrays but not yet dynamic.</li>
       <li>2020-12-12: Support for senses. Wow - that was INCREDIBLY complex.</li>
