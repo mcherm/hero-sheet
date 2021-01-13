27e2eb95f1200ea7a4966f9754e2e0ae0faa7d06
Michael Chermside <mcherm@mcherm.com>
2021-01-13 10:50:42 -0500

    Turned the activation horizontal within the ActivationPanel.

--- src/components/ActivationWidget.vue
+++ src/components/ActivationWidget.vue
@@ -2,53 +2,55 @@
   A control that is used to turn powers on and off.
 -->
 <template>
-  <div class="activate-widget column">
+  <div :class="{'activate-widget': true, 'horizontal': horizontal}">
     <svg
-        xmlns='http://www.w3.org/2000/svg'
-        width='16'
-        height='32'
-        viewBox='0 0 512 1024'
+        xmlns="http://www.w3.org/2000/svg"
+        :width="horizontal ? 32 : 16"
+        :height="horizontal ? 16 : 32"
+        :viewBox="horizontal ? '0 0 1024 512' : '0 0 512 1024'"
     >
-      <rect class="outside" x="20" y="20" width="472" height="720" rx="80"/>
-      <g class="on-symbol" v-if="activation.activationStatus === 'on'">
-        <circle class="edge" r="150" cx="256" cy="230"/>
-        <line x1="256" :y1="230-60" x2="256" :y2="230+60"/>
-        <line :x1="256 - 60" y1="230" :x2="256 + 60" y2="230"/>
-        <line x1="80" y1="500" x2="432" y2="500"/>
-        <line x1="80" y1="580" x2="432" y2="580"/>
-        <line x1="80" y1="660" x2="432" y2="660"/>
+      <g :transform="horizontal ? 'rotate(-90) translate(-512,0)' : ''">
+        <rect class="outside" x="20" y="20" width="472" height="720" rx="80"/>
+        <g class="on-symbol" v-if="activation.activationStatus === 'on'">
+          <circle class="edge" r="150" cx="256" cy="230"/>
+          <line x1="256" :y1="230-60" x2="256" :y2="230+60"/>
+          <line :x1="256 - 60" y1="230" :x2="256 + 60" y2="230"/>
+          <line x1="80" y1="500" x2="432" y2="500"/>
+          <line x1="80" y1="580" x2="432" y2="580"/>
+          <line x1="80" y1="660" x2="432" y2="660"/>
+        </g>
+        <g class="off-symbol" v-if="activation.activationStatus === 'off'">
+          <circle class="edge" r="150" cx="256" cy="530"/>
+          <line :x1="256 - 60" y1="530" :x2="256 + 60" y2="530"/>
+          <line x1="80" y1="100" x2="432" y2="100"/>
+          <line x1="80" y1="180" x2="432" y2="180"/>
+          <line x1="80" y1="260" x2="432" y2="260"/>
+        </g>
+        <g class="partial" v-if="activation.activationStatus === 'partial'">
+          <polygon points="80,1003 256,803 432,1003"/>
+          <line x1="80" y1="100" x2="432" y2="100"/>
+          <line x1="80" y1="180" x2="432" y2="180"/>
+          <line x1="80" y1="260" x2="432" y2="260"/>
+          <line x1="80" y1="340" x2="432" y2="340"/>
+          <line x1="80" y1="420" x2="432" y2="420"/>
+          <line x1="80" y1="500" x2="432" y2="500"/>
+          <line x1="80" y1="580" x2="432" y2="580"/>
+          <line x1="80" y1="660" x2="432" y2="660"/>
+        </g>
+        <g v-if="canBePartial">
+          <line x1="256" y1="740" x2="256" y2="803"/>
+          <line x1="256" y1="803" x2="80" y2="1003"/>
+          <line x1="256" y1="803" x2="432" y2="1003"/>
+          <line x1="80" y1="1003" x2="432" y2="1003"/>
+        </g>
+        <rect class="click-catcher" x="4" y="4" width="504" height="752" rx="96" @click="clickOnOff()"/>
+        <rect class="click-catcher" x="4" y="787" width="504" height="232" rx="96" @click="clickPartial()"/>
       </g>
-      <g class="off-symbol" v-if="activation.activationStatus === 'off'">
-        <circle class="edge" r="150" cx="256" cy="530"/>
-        <line :x1="256 - 60" y1="530" :x2="256 + 60" y2="530"/>
-        <line x1="80" y1="100" x2="432" y2="100"/>
-        <line x1="80" y1="180" x2="432" y2="180"/>
-        <line x1="80" y1="260" x2="432" y2="260"/>
-      </g>
-      <g class="partial" v-if="activation.activationStatus === 'partial'">
-        <polygon points="80,1003 256,803 432,1003"/>
-        <line x1="80" y1="100" x2="432" y2="100"/>
-        <line x1="80" y1="180" x2="432" y2="180"/>
-        <line x1="80" y1="260" x2="432" y2="260"/>
-        <line x1="80" y1="340" x2="432" y2="340"/>
-        <line x1="80" y1="420" x2="432" y2="420"/>
-        <line x1="80" y1="500" x2="432" y2="500"/>
-        <line x1="80" y1="580" x2="432" y2="580"/>
-        <line x1="80" y1="660" x2="432" y2="660"/>
-      </g>
-      <g v-if="canBePartial">
-        <line x1="256" y1="740" x2="256" y2="803"/>
-        <line x1="256" y1="803" x2="80" y2="1003"/>
-        <line x1="256" y1="803" x2="432" y2="1003"/>
-        <line x1="80" y1="1003" x2="432" y2="1003"/>
-      </g>
-      <rect class="click-catcher" x="4" y="4" width="504" height="752" rx="96" @click="clickOnOff()"/>
-      <rect class="click-catcher" x="4" y="787" width="504" height="232" rx="96" @click="clickPartial()"/>
     </svg>
     <div class="active-ranks-setting" v-if="activation.activationStatus === 'partial'">
-      <edit-button class="active-ranks-button top" :onClick="() => clickIncr()">+</edit-button>
+      <edit-button class="active-ranks-button incr" :onClick="() => clickIncr()">+</edit-button>
       <number-display class="active-ranks" :value="activation.ranks" :used-in-play="true"/>
-      <edit-button class="active-ranks-button bottom" :onClick="() => clickDecr()">-</edit-button>
+      <edit-button class="active-ranks-button decr" :onClick="() => clickDecr()">-</edit-button>
     </div>
   </div>
 </template>
@@ -74,6 +76,7 @@
     props: {
       activation: { "type": Object, "required": true },
       canBePartial: { "type": Boolean, "required": true },
+      horizontal: { "type": Boolean, "required": false, default: false },
     },
     methods: {
       clickOnOff: function() {
@@ -132,10 +135,16 @@
     justify-content: center;
     align-items: center;
   }
+  .activate-widget.horizontal {
+    flex-direction: row;
+  }
   .active-ranks-setting {
     display: flex;
     flex-direction: column;
     align-items: stretch;
+  }
+  .horizontal > .active-ranks-setting {
+    flex-direction: row;
   }
   .active-ranks {
     border: solid var(--box-border-color) 1px;
@@ -147,10 +156,18 @@
     padding: 0;
     margin: 0;
   }
-  button.active-ranks-button.top {
+  button.incr {
     border-radius: 5px 5px 0 0;
   }
-  button.active-ranks-button.bottom {
+  button.decr {
     border-radius: 0 0 5px 5px;
+  }
+  .horizontal > .active-ranks-setting > button.incr {
+    border-radius: 5px 0 0 5px;
+    padding: 0 1px;
+  }
+  .horizontal > .active-ranks-setting > button.decr {
+    border-radius: 0 5px 5px 0;
+    padding: 0 1px;
   }
 </style>
